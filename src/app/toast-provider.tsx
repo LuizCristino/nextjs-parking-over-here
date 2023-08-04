37abee5a948d0013639d3f42e140d5d7bad49845
Luiz Cristino <luizptcristino@gmail.com>
2023-08-04 10:55:18 -0400

    refactor: toast provider

--- src/app/toast-provider.tsx
+++ src/app/toast-provider.tsx
@@ -8,12 +8,21 @@ export type ToastProviderProps = {
   children?: ReactNode;
 };
 
+/**
+ * The only reason that toast provider is in a
+ * different component than the other providers
+ * is so it can access the session and position
+ * itself correctly based if the header exists or
+ * not
+ */
 export function ToastProvider(props: ToastProviderProps) {
   const { children } = props;
 
   const session = useSession();
 
-  const className = session.data?.user != null ? 'mt-[64px]' : undefined;
+  const className = `transition-transform transition-duration-[750ms] ${
+    session.data?.user != null ? 'translate-y-[64px]' : ''
+  }`;
 
   return (
     <>
